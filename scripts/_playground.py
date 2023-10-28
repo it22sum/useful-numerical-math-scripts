6e575966ac4fce8b94a4c7f1a720f08265f07704
fiechdus <dustin.fiechter@protonmail.ch>
2023-10-28 16:06:31 +0200

    Added playground script that can be used to temporarily test out code.

---
+++ scripts/_playground.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import from other files:
+from sekanten_verfahren import sekanten_verfahren
+
+
+# ========================================================
+# Idea: use this file to 'play around' and try things out.
+# It is supposed to be used as a sort of test environment
+# ========================================================
+
+
