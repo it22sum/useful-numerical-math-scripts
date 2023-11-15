01002622c72d1019bfc8e0313db14febb632fdb1
fiechdus <dustin.fiechter@protonmail.ch>
2023-11-15 23:14:55 +0100

    Minor comment changes.

--- scripts/_playground.py
+++ scripts/_playground.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Import from other files:
-from sekanten_verfahren import sekanten_verfahren
+#from sekanten_verfahren import sekanten_verfahren
 
 
 # ========================================================
 # Idea: use this file to 'play around' and try things out.
-# It is supposed to be used as a sort of test environment
+# It is supposed to be used as a sort of test environment.
 # ========================================================
 
 
