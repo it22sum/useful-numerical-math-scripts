da75bf8a944ba30738c37a415d6e62535065d7a3
fiechdus <dustin.fiechter@protonmail.ch>
2023-12-11 17:18:37 +0100

    Added #3 sekanten_verfahren definition in jupyter

--- scripts/kap3/sekanten_verfahren.py
+++ scripts/kap3/sekanten_verfahren.py
@@ -10,17 +10,9 @@ def sekanten_verfahren(f, x0, x1, tolerance):
     #            => Wert ist genug genau, um als "richtige" Lösung
     #               akzeptiert zu werden.
 
-    while f(x_n - tolerance) * f(x_n + tolerance) >= 0:  # abs(f(x_n)) >= tolerance:
+    while abs(f(x_n)) >= tolerance:
         x2 = x_n - ((x_n - x_n_minus_1) / (f(x_n) - f(x_n_minus_1))) * f(x_n)
         x_n_minus_1 = x_n
         x_n = x2
 
     return x_n
-
-
-# Hier Funktion definieren:
-def funktion(x):
-    return x ** 3 + 3 * x ** 2 + 12 * x + 8
-
-
-print("Nullstelle: " + str(sekanten_verfahren(funktion, -5, 5, 0.01)))
