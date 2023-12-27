f63ba02e535f4bfb7f73f6db2f07971186546723
Newoss <91864212+Newoss@users.noreply.github.com>
2023-12-27 15:03:19 +0100

    Update berechnung_konditionszahl.py
    berechnung_konditionszahl erweitert:
    Division durch Null 
    e Funktionen funktionieren jetzt auch

--- scripts/kap2/berechnung_konditionszahl.py
+++ scripts/kap2/berechnung_konditionszahl.py
@@ -1,9 +1,28 @@
-def konditionszahl(f, x):
-    from sympy import symbols, diff
-    x_symbol = symbols('x')
-    derivative = diff(f, x_symbol)
+import sympy as sp
 
-    f_value = f.subs(x_symbol, x)
-    derivative_value = derivative.subs(x_symbol, x)
+def berechne_konditionszahl(funktion_str, x_0):
+    # Definieren des symbolischen Variablen
+    x = sp.symbols('x')
 
-    return (abs(derivative_value) * abs(x)) / abs(f_value)
+    # Umwandeln des Funktions-Strings in eine symbolische Funktion
+    funktion = sp.sympify(funktion_str,locals={'e': sp.E})
+
+    # Berechnen der Ableitung der Funktion
+    funktion_prime = sp.diff(funktion, x)
+
+    # Berechnen der Konditionszahl für den Punkt x_0
+    if funktion.subs(x, x_0) != 0:
+        konditionszahl = abs(x_0 * funktion_prime.subs(x, x_0) / funktion.subs(x, x_0))
+
+    else:
+        return "Fehler: Division durch Null"
+
+    return konditionszahl
+
+
+# Beispielanwendung
+funktion_str = "1 - e**x"  # Funktion als String
+x_0 = 1  # Punkt, an dem die Konditionszahl berechnet werden soll
+
+konditionszahl = berechne_konditionszahl(funktion_str, x_0)
+print("Konditionszahl der Funktion", funktion_str, "bei x =", x_0, ":", konditionszahl)
